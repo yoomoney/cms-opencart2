ee9046412351b8e6f28fd028fdd8add3b3206e42
aalopatin <aalopatin@yamoney.ru>
2018-09-28 10:56:40 +0300

    Добавлено приглашение пройти опрос

--- catalog/model/extension/payment/yandex_money.php
+++ catalog/model/extension/payment/yandex_money.php
@@ -12,7 +12,7 @@ require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'yandex_money'.DIRECTORY_SEPA
  */
 class ModelExtensionPaymentYandexMoney extends Model
 {
-    const MODULE_VERSION = '1.1.2';
+    const MODULE_VERSION = '1.1.3';
 
     private $kassaModel;
     private $walletModel;
