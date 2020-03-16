9b6c5cc36fb98a14feebac642086476f710e18f1
Павел А. Молодовский <pamolodovskii@yamoney.ru>
2020-03-16 12:33:02 +0300

    Добавлен платежный виджет в список платежных методов.

--- admin/language/en-gb/extension/payment/yandex_money.php
+++ admin/language/en-gb/extension/payment/yandex_money.php
@@ -43,6 +43,7 @@ $_['kassa_payment_method_yandex_money'] = 'Yandex.Money';
 $_['kassa_payment_method_mobile']       = 'Direct carrier billing';
 $_['kassa_payment_method_installments'] = 'Installments';
 $_['kassa_payment_method_tinkoff_bank'] = 'Tinkoff online banking';
+$_['kassa_payment_method_widget']       = 'Payment widget from Yandex.Checkout (cards, Apple Pay and Google Play)';
 
 $_['kassa_payment_method_error_required'] = 'Please select at least one option from the list';
 
