7fa5a8518f67190cb9f14f7e1bad5998c36a7bbb
pamolodovskii <pamolodovskii@yamoney.ru>
2020-04-21 20:30:11 +0300

    Изменено название платежного метода(виджет)

--- admin/model/extension/payment/yandex_money.php
+++ admin/model/extension/payment/yandex_money.php
@@ -2,7 +2,7 @@
 
 class ModelExtensionPaymentYandexMoney extends Model
 {
-    const MODULE_VERSION = '1.4.1';
+    const MODULE_VERSION = '1.4.2';
 
     private $kassaModel;
     private $walletModel;
--- catalog/model/extension/payment/yandex_money/Model/KassaSecondReceiptModel.php
+++ catalog/model/extension/payment/yandex_money/Model/KassaSecondReceiptModel.php
@@ -17,7 +17,7 @@ use YandexCheckout\Request\Receipts\ReceiptResponseItemInterface;
 
 class KassaSecondReceiptModel
 {
-    const MODULE_VERSION = '1.4.1';
+    const MODULE_VERSION = '1.4.2';
 
     /**
      * @var \Config
