c547d8b4ca41c91c44bd6860b5aa179252ad936a
Антон Н. Николаев <nikolaevanym@yamoney.ru>
2020-04-30 16:59:18 +0300

    Совместимость с PHP 5.4

--- admin/model/extension/payment/yandex_money.php
+++ admin/model/extension/payment/yandex_money.php
@@ -2,7 +2,7 @@
 
 class ModelExtensionPaymentYandexMoney extends Model
 {
-    const MODULE_VERSION = '1.4.3';
+    const MODULE_VERSION = '1.4.4';
 
     private $kassaModel;
     private $walletModel;
