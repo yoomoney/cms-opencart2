7c94da2e0c543f7f4b057d3930b2f063082be1ea
Антон Н. Николаев <nikolaevanym@yoomoney.ru>
2021-04-15 17:54:28 +0300

    Проверка на наличие jQuery.fn.button

--- catalog/model/extension/payment/yoomoney.php
+++ catalog/model/extension/payment/yoomoney.php
@@ -26,7 +26,7 @@ require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'yoomoney'.DIRECTORY_SEPARATO
  */
 class ModelExtensionPaymentYoomoney extends Model
 {
-    const MODULE_VERSION = '2.0.9';
+    const MODULE_VERSION = '2.0.10';
 
     private $kassaModel;
     private $walletModel;
